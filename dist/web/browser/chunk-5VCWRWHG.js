3b7cc14a2c2b06f98508e93895c3ac4ef08e4939
DimpleBamaniya <companytestmail99@gmail.com>
2025-02-18 12:08:00 +0530

    Delete dist/web directory

--- dist/web/browser/chunk-5VCWRWHG.js
+++
@@ -1,1 +0,0 @@
-import{d as a,e as b}from"./chunk-Z4UV2KRT.js";import"./chunk-7XQF5X2M.js";export{b as UserModule,a as routes};
--- dist/web/browser/chunk-DLOT72US.js
+++
@@ -1,1 +0,0 @@
-import{a,b}from"./chunk-JQDJDMN3.js";import"./chunk-7XQF5X2M.js";export{b as ProductModule,a as routes};
